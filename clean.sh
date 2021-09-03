7c111b4c0b0080b400b5d5efa6df192adf493009
inna <ikunygina@yandex.ru>
2021-09-03 23:06:34 +0300

    start of using github workflow

---
+++ clean.sh
@@ -0,0 +1,8 @@
+#autopep8 ./ --recursive --in-place -a
+#yapf -ir .
+#autoflake --in-place --remove-all-unused-imports --remove-unused-variables -r ./
+black .
+mypy .
+flake8 .
+isort --check .
+py.test tests
