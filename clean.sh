2d462dd926c638cf69fba7ee28f1d943729274e8
inna <ikunygina@yandex.ru>
2021-09-09 20:26:18 +0300

    for checkout

--- clean.sh
+++ clean.sh
@@ -1,9 +1,15 @@
 #autopep8 ./ --recursive --in-place -a
 #yapf -ir .
 #autoflake --in-place --remove-all-unused-imports --remove-unused-variables -r ./
+echo STARTING BLACK
 black .
+echo STARTING MYPY
 mypy .
+echo STARTING FLAKE8
 flake8 .
+echo STARTING CHECK ISORT
 isort --check .
+echo STARTING ISORT
 isort .
+echo RUNNING TESTS
 py.test tests
