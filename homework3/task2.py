3def6dad43e730f3ccff1a371054983f612b3e46
inna <ikunygina@yandex.ru>
2021-09-09 23:17:52 +0300

    commit for checkout

--- homework3/task2.py
+++ homework3/task2.py
@@ -2,8 +2,7 @@ import time
 import struct
 import random
 import hashlib
-from multiprocessing.pool import ThreadPool
-from multiprocessing import Process, Pool
+from multiprocessing import Pool
 
 
 def slow_calculate(value):
@@ -12,36 +11,9 @@ def slow_calculate(value):
     data = hashlib.md5(str(value).encode()).digest()
     return sum(struct.unpack('<' + 'B' * len(data), data))
 
-# Calculate total sum of slow_calculate() of all numbers starting from 0 to 500.
-# Calculation time should not take more than a minute. Use functional capabilities of multiprocessing module.
-# You are not allowed to modify slow_calculate function.
 
-
-def sum_slow_calculate_from0_to500():
-    # return sum(Process(target=slow_calculate, args=(i,)) for i in range(500))
-    # slow_calculate_result = []
-    # pool = ThreadPool(processes=1)
-    # for i in range(5):
-    #     p = Process(target=slow_calculate, args=(i,))
-    #     print(p)
-    #     slow_calculate_result.append(p)
-    #     p.start()
-    #
-    # for elem in slow_calculate_result:
-    #     print(elem.get())
-
-    #
-    # with Pool() as pool:
-    #     results = [pool.apply_async(slow_calculate, (i,)) for i in range(5)]
-    #     pool.close()
-    #     pool.join()
-    #
-    # results = [result.get() for result in results]
-    # print(results)
-    with Pool(8) as pool:
+def sum_slow_calculate_from_0_to_500():
+    """Summa 500 calls slow_calculate"""
+    with Pool(32) as pool:
         answer = sum(pool.map(slow_calculate, list(range(500))))
-    print(answer)
-
-
-print(sum_slow_calculate_from0_to500())
-print(slow_calculate(1))
+    return answer
---
+++ tests/homework3/test_hw2_task2.py
@@ -0,0 +1,5 @@
+from homework3.task2 import sum_slow_calculate_from_0_to_500
+
+
+def test_how_fast():
+    ...
