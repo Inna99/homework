91508392104491bd6e69617ace4c882030b14df0
Inna99 <ikunygina@yandex.ru>
2021-10-01 22:21:27 +0300

    task1

--- homework3/task1.py
+++ homework3/task1.py
@@ -1,32 +1,3 @@
-# In previous homework task 4, you wrote a cache function that remembers other function output value.
-# Modify it to be a parametrized decorator, so that the following code:
-#
-# @cache(times=3)
-# def some_function():
-#     pass
-# Would give out cached value up to times number only. Example:
-#
-# @cache(times=2)
-# def f():
-#     return input('? ')   # careful with input() in python2, use raw_input() instead
-#
-# >>> f()
-# ? 1
-# '1'
-# >>> f()     # will remember previous value
-# '1'
-# >>> f()     # but use it up to two times only
-# '1'
-# >>> f()
-# ? 2
-# '2'
-# 3-1 странный тест как по мне.. проверяет последовательность принтов а не логику функции.
-# я думаю стоит завернуть функцию которая делает return list() или еще какойто объект и
-# проверять ссылки через is. на первом вызове запомнила ссылку, следующие 4 возвращало ее же на 5ом она сменилась.
-# ну и с другим параметром потом. и собственно потому что ты возвращаемый результат не проверяла ты пропустила что у
-# тебя возвращается список результат функции + количество повторений а не сам результат.
-# да и в примере там знак вопроса это в оборачиваемой функции запрос инпута.
-# чтоб наглядно показать что инпут то вызывается то не вызывается, а не отладочная информация кеша)
 import functools
 from typing import Callable, Dict
 
@@ -55,8 +26,3 @@ def cache(times: int = 5):
         return wrapper
 
     return one_more_func
-
-
-@cache(times=2)
-def func(a, b):
-    return (a + b) ** 2
--- tests/homework3/test_hw3_task1.py
+++ tests/homework3/test_hw3_task1.py
@@ -1,19 +1,25 @@
-from homework3.task1 import func
+from homework3.task1 import cache
 
 
 def test_cache_func():
-    """Check that funcs are equal"""
-    reference_1 = func(1, 1)
-    reference_2 = func(1, 1)
+    """checks that the 'times' times function is cached"""
+
+    @cache(times=1)
+    def test():
+        return list()
+
+    reference_1 = test()
+    reference_2 = test()
+    reference_3 = test()
     assert reference_1 is reference_2
+    assert reference_2 is not reference_3
 
 
-def test_cache_func_negativ():
-    """Check that funcs are not equal"""
-    one, two, three, four = (  # noqa
-        func(1, 1),
-        func(1, 1),
-        func(1, 1),
-        func(1, 1)
-    )
-    assert id(one) == id(four)
+def test_result():
+    """checks that the return values of the function and the function under the decorator are equal"""
+
+    def test():
+        return list()
+
+    decorated = cache(1)(test)
+    assert decorated() == test()
